3ca9cb1b4793397e8ad32d81934989835adae9c8
olef5735 <ol.schiza@outlook.com>
2023-05-09 15:27:30 +0300

    files

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Trying Gatby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
